0ebbd6f6877f06e9879aac36b6872e3a4f575066
Wielfried Zouantcha <44246692+zouantchaw@users.noreply.github.com>
2021-11-01 17:57:58 +0000

    add two other solutions

--- index.js
+++ index.js
@@ -11,4 +11,18 @@ function reverseString(str) {
   return newArr.join("");
 }
 
+function reverseString2(str) {
+  for (var reversedStr = "", i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
+}
+
+function reverseString3(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
+
 reverseString("hello");
